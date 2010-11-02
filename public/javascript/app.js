f39bc2a188b8a2821bf9f14dc4a6a84398560148
Fritz Thielemann <fritz.thielemann@gmail.com>
2010-11-02 17:47:06 +0100

    some more changes

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,21 +1,86 @@
-$(document).ready(function() {
-	//Set default open/close settings
-	$('.acc_container').hide(); //Hide/close all containers
-	$('.acc_trigger:first').addClass('active').next().show(); //Add "active" class to first trigger, then show/open the immediate next container
-
-	//On Click
-	$('.acc_trigger').click(function(){
-		if( $(this).next().is(':hidden') ) { //If immediate next container is closed...
-			$('.acc_trigger').removeClass('active').next().slideUp(); //Remove all "active" state and slide up the immediate next container
-			$(this).toggleClass('active').next().slideDown(); //Add "active" state to clicked trigger and slide down the immediate next container
-		}
-		return false; //Prevent the browser jump to the link anchor
-	});
-});
+function despamitizeEAddress() {
+  var parts = ["i", "n", "f", "o", "fournova", "com"];
+  var address = "";
+  for(var i=0; i<parts.length; i++){
+    if(i == (parts.length-2)){
+      address += "@"+parts[i];
+    }else if(i == (parts.length-1)){
+      address += "."+parts[i];
+    }else{
+      address += parts[i];
+    }
+  }
+  return address;
+}
 
 $(function(){
-	$('#header .contact a').click(function(){
-		$('.vcard').slideToggle();
-	})
+  // margin for logo
+  var logoMargin = ($(window).height()/3)-(parseInt($("#logoContainer").css('height'))/2);
+  $("#pageContent").css('paddingTop', logoMargin+'px');
+  $("#contentWrapper").css('backgroundPosition', '50px '+(logoMargin-70)+'px');
+  
+  // event bindings
+  $('#contactLink').click(function(event){
+      $('#imprint').hide();
+      $('#legalDisclaimer').hide();
+      $('#contact').show();
+      event.currentTarget.blur();
+    });
+    
+  $('#imprintLink').click(function(event){
+      $('#contact').hide();
+      $('#legalDisclaimer').hide();
+      $('#imprint').show();
+      $('#formMessage').hide();
+      event.currentTarget.blur();
+    });
+    
+  $('#legalDisclaimerLink').click(function(event){
+      $('#legalDisclaimer').toggle();
+      $(event.currentTarget).find("a").blur();
+      $('#legalDisclaimerLink').toggleClass('collapsibleHeaderClosed');
+      $('#legalDisclaimerLink').toggleClass('collapsibleHeaderOpen');
+    });
+    
+  $('#logo').click(function(event){
+      $('#contact').hide();
+      $('#legalDisclaimer').hide();
+      $('#imprint').hide();
+      $('#formMessage').hide();
+      event.currentTarget.blur();
+    });
+    
+  // substitute email link
+  $('#emailLink').html("<a href='mailto:"+despamitizeEAddress()+"'>Email</a>");
+    
+  // contact form submission
+  $('#contactForm').submit(function(event){
+    var name = $('#formFieldName').attr('value');
+    var email = $('#formFieldEmail').attr('value');
+    var phone = $('#formFieldPhone').attr('value');
+    var message = $('#formFieldMessage').attr('value');
+    if(name == false || email == false || message == false) {
+          // show error message
+        $('#formMessage').text("Please fill out all required fields.").show();
+     }else{
+       // send form
+       $('#formMessage').hide();
+       $('#sendButton').hide();
+       $('#loaderWheel').show();
+       $.post("sendMail.php", { name: name, email: email, message: message, phone: phone },
+          function(data){
+            $('#formMessage').text(data).show();
+            $('#loaderWheel').hide();
+            $('#sendButton').show();
+          });
+     }
+     
+     event.currentTarget.blur();
+     // the data is sent via ajax
+     event.preventDefault();
+  });
+  
+    
+  // show content only now, when everything is set up
+  $("#contentWrapper").show();
 });
-
