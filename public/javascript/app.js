13c2953cd5726d6ae874f36d13354b71f21081ef
Fritz Thielemann <fritz.thielemann@gmail.com>
2011-02-12 18:25:13 +0100

    reworked for euruko 2011 ad

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,3 +1,15 @@
+function getLang() {
+	if (navigator.appName == 'Netscape')
+	  var language = navigator.language;
+	else
+	  var language = navigator.browserLanguage;
+
+	if (language.indexOf('en') > -1) document.location.href = 'en/index.html';
+	else if (language.indexOf('de') > -1) document.location.href = 'de/index.html';
+	else 
+	  document.location.href = 'en/index.html';
+}
+
 function despamitizeEAddress() {
   var parts = ["i", "n", "f", "o", "cileos", "com"];
   var address = "";
